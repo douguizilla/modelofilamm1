9a2d45045005c0c7245a9d2ab28a95ee68dcd255
Douglas <douglasgommez@gmail.com>
2021-05-02 12:00:05 -0300

    Initial commit

---
+++ src/main/kotlin/Dados.kt
@@ -0,0 +1,6 @@
+/*
+    Trabalho 1 - Modelo de fila M/M/1
+    Aluno: Douglas Gomes de Paula
+    Matrícula: 11621BCC013
+ */
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+/*
+    Trabalho 1 - Modelo de fila M/M/1
+    Aluno: Douglas Gomes de Paula
+    Matrícula: 11621BCC013
+ */
+import kotlin.random.Random
+
+fun main(){
+    for(i in 1..1000)
+        println(Random.nextDouble(0.0,1.0))
+}
+
+fun menu(){
+    showOptions()
+}
+
+fun showOptions() {
+    TODO("Mostrar as opções disponiveis para manipulação do sistema")
+}
